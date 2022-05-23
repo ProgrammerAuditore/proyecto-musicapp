b5665be0e02ab143b6541ba0d7dbb8d61f7f3dd8
ProgrammerAuditore <victorvj098@gmail.com>
2022-05-23 06:23:22 -0500

    *** add-express-handlebars : Se implemento express-handlebars

--- src/index.js
+++ src/index.js
@@ -4,10 +4,24 @@ const express = require('express');
 const _conexion = require('./database');
 const indexRouter = require('./routes/index.routes');
 const cancionRouter = require('./routes/cancion.routes');
+const exphbs = require("express-handlebars");
 const path = require('path');
 const app  = express();
 
 app.set('port', process.env.APP_PORT || 3000);
+
+app.set('views', path.join(__dirname, "views"));
+
+// * Motor de plantillas
+app.engine(".hbs", exphbs.engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    extname: '.hbs'
+}));
+
+app.set("view engine", ".hbs");
+
 
 // * Database
 _conexion();
