e4f898b4bf7057b5a6eab3e0981950989a291135
ProgrammerAuditore <victorvj098@gmail.com>
2023-04-06 21:31:29 -0500

    ***** dev-vagrant : Se agrego vagrant y script de ejecucion

---
+++ vagrant.sh
@@ -0,0 +1,12 @@
+# Este es un script para Vagranfile
+# vagrant provision
+sudo rm -fr /home/vagrant/data
+sudo mkdir -p /home/vagrant/data
+echo "[...] Provision iniciando [...]"
+cd /home/vagrant/workspace
+ls -lsa
+docker-compose stop
+docker-compose rm --force
+docker-compose build
+docker-compose up -d
+echo "[...] Provision finalizado exitosamente ! [...]"
